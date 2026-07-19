df223578b963191cccc41f2c7ea648611fd5d890
Ddfathu <108730817+Ddfathu@users.noreply.github.com>
2026-07-19 10:01:35 +0700

    Manage via Bot

---
+++ app/menus/util.js
@@ -0,0 +1,14 @@
+<pre>// app/menus/util.js
+
+export function displayHtml(htmlString) {
+  if (!htmlString) return "";
+  return htmlString.replace(/<[^>]*>?/gm, '').trim();
+}
+
+export function formatQuotaByte(bytes) {
+  if (bytes === 0) return '0 B';
+  const k = 1024;
+  const sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+  const i = Math.floor(Math.log(bytes) / Math.log(k));
+  return parseFloat((bytes / Math.pow(k, i)).toFixed(2)) + ' ' + sizes[i];
+}</pre>
